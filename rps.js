7e8086e7013fb3cde40608d989da1555035f6870
Brandon <mauldinbg@gmail.com>
2023-04-10 04:14:26 -0500

    Add working 5 round rps script

--- rps.js
+++ rps.js
@@ -1,6 +1,10 @@
 let choiceInt;
-let playerSelection = prompt("Type Rock, Paper, or Scissors.", "");
+let playerSelection;
 let computerSelection;
+let result=0;
+let rounds=0;
+var wins=0;
+var losses=0;
 
 function random() {
     return Math.floor((Math.random() * 3)+1)
@@ -12,14 +16,56 @@ function randomizeComputerChoice() {
 
 function getComputerChoice() {
     if (choiceInt==1) {
-        computerSelection='Rock';
+        computerSelection='rock';
     } else if (choiceInt==2) {
-        computerSelection='Paper';
+        computerSelection='paper';
     } else {
-        computerSelection='Scissors';
+        computerSelection='scissors';
     }
 }
 
-randomizeComputerChoice();
-getComputerChoice();
-console.log(computerSelection);
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Type Rock, Paper, or Scissors.", "").toLowerCase();
+    if (playerSelection==computerSelection) {
+        console.log("You tied this round!");
+    }   else if (playerSelection=="rock" && computerSelection=="scissors") {
+        console.log("You win this round!");
+        wins+=1;
+    }   else if (playerSelection=="scissors" && computerSelection=="paper") {
+        console.log("You win this round!");
+        wins+=1;
+    }   else if (playerSelection=="paper" && computerSelection=="rock") {
+        console.log("You win this round!");
+        wins+=1;
+    }   else if (computerSelection=="rock" && playerSelection=="scissors") {
+        console.log("You lose this round!");
+        losses+=1;
+    }   else if (computerSelection=="scissors" && playerSelection=="paper") {
+        console.log("You lose this round!");
+        losses+=1;
+    }   else if (computerSelection=="paper" && playerSelection=="rock") {
+        console.log("You lose this round!");
+        losses+=1;
+    }   else {
+        console.log("Try again.");
+    }
+}
+
+function game() {
+    for (let rounds=1; rounds<=5; rounds++) {
+        randomizeComputerChoice();
+        getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+    if (wins > losses) {
+            console.log('You beat the computer!');
+        } else if (losses > wins) {
+            console.log('The computer won the game!');
+        } else {
+            console.log('Nobody won!');
+        }
+     }
+    
+
+
+game();
